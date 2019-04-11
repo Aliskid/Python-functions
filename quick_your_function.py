127a50395f7fdd6efd9531f515484a037d292eb0
Alison <33938391+Aliskid@users.noreply.github.com>
2019-04-11 10:32:57 +0200

    Create quick_your_function.py

---
+++ quick_your_function.py
@@ -0,0 +1,14 @@
+def multiprocessing_functions():
+
+	''' Intermediate function to speed your function's execution
+	by using multiprocessing '''
+
+	manager = multiprocessing.Manager()
+	info_dict = manager.dict()
+	jobs = []
+	p = multiprocessing.Process(target=your_function, args=(...))
+	jobs.append(p)
+	p.start()
+	for proc in jobs:
+    proc.join()
+	return info_dict
